d93470ad4652f0c36b22ae17dc424c678c1d01ec
Addam <addamdearaujo@gmail.com>
2023-04-01 20:27:30 -0300

    created first animation at button "add task"

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let toDo = document.getElementById("to-do");
-let texto = document.getElementById("texto");
-let botao = document.getElementById("botao");
-let tarefas = document.getElementById("tarefas");
+let text = document.getElementById("text");
+let button = document.getElementById("button");
+let tasks = document.getElementById("tasks");
 let index = 0;
 
-function addTarefa() {
-  let novoElemento = document.createElement("div");
-  novoElemento.innerText = texto.value;
-  novoElemento.id = `id${++index}`;
-  let novoBotao = document.createElement("button");
-  novoBotao.innerText = "remover tarefa";
-  novoBotao.id = `id${index}`;
-  novoBotao.setAttribute("onclick", "removerTarefa(event)");
-  tarefas.appendChild(novoElemento);
-  novoElemento.appendChild(novoBotao);
+function addTask() {
+  let newElement = document.createElement("div");
+  newElement.innerText = text.value;
+  newElement.id = `id${++index}`;
+  let newButton = document.createElement("button");
+  newButton.innerText = "remove task";
+  newButton.id = `id${index}`;
+  newButton.setAttribute("onclick", "removeTask(event)");
+  tasks.appendChild(newElement);
+  newElement.appendChild(newButton);
 }
 
-function removerTarefa(event) {
-  let novoElemento = document.getElementById(event.target.id);
-  novoElemento.parentNode.removeChild(novoElemento);
+function removeTask(event) {
+  let newElement = document.getElementById(event.target.id);
+  newElement.parentNode.removeChild(newElement);
 }
 
 toDo.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (!texto.value) {
-    alert("Insira uma tarefa!");
+  if (!text.value) {
+    alert("Enter a task!");
     return;
   }
-  addTarefa();
-  texto.value = "";
+  addTask();
+  text.value = "";
 });
